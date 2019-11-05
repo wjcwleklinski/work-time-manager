3e6405c1cc38493fe176e65cf191a43a5e166a0d
Wojtek <wjcwleklinski@gmail.com>
2019-11-05 12:03:23 +0100

    Initial commit

---
+++ src/test/java/com/wjcwleklinski/worktimemanager/WorkTimeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjcwleklinski.worktimemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTimeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
