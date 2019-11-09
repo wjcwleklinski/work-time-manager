af44d74f0c4e6f6f7be35d01658b297c708b2723
Wojtek <wjcwleklinski@gmail.com>
2019-11-09 11:02:07 +0100

    Entities correction

--- src/main/java/com/wjcwleklinski/worktimemanager/repositories/EmployeeDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wjcwleklinski.worktimemanager.repositories;
-
-import com.wjcwleklinski.worktimemanager.entity.EmployeeDetails;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface EmployeeDetailsRepository extends CrudRepository<EmployeeDetails,Long> {
-}
--- src/main/java/com/wjcwleklinski/worktimemanager/repositories/ProjectDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wjcwleklinski.worktimemanager.repositories;
-
-import com.wjcwleklinski.worktimemanager.entity.ProjectDetails;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface ProjectDetailsRepository extends CrudRepository<ProjectDetails,Long> {
-}
